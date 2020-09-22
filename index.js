c0cad7b7d0a0de439b1eb6b53cbaf8804928a9f3
Mike Bloom <1985850+bloommi@users.noreply.github.com>
2020-09-22 19:23:27 -0400

    Add radar imagery layer provided by NOAA

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ import {Map, View} from 'ol';
 import TileLayer from 'ol/layer/Tile';
 import OSM from 'ol/source/OSM';
 import {fromLonLat} from 'ol/proj';
+import Image from 'ol/layer/Image';
+import ImageWMS from 'ol/source/ImageWMS';
 
 const washingtonLonLat = [-77.036667, 38.895];
 const washingtonLonLatMercator = fromLonLat(washingtonLonLat);
@@ -18,3 +20,20 @@ const map = new Map({
     zoom: 6
   })
 });
+
+var source = new ImageWMS({
+    attributions: ['NOAA'],
+    url: 'https://nowcoast.noaa.gov/arcgis/services/nowcoast/radar_meteo_imagery_nexrad_time/MapServer/WMSServer',
+    params: {'LAYERS': '1'},
+    projection: 'EPSG:3857'
+});
+
+var layer = new Image({
+    title: 'NOAA Radar',
+    zIndex: 1,
+    visible: true,
+    source: source,
+    opacity: 0.7
+});
+
+map.addLayer(layer);
